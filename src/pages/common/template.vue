303742b3a572d8d7b57195cb3d1ac82e8ddeddb4
lijixuan <15201002062@163.com>
2023-07-29 10:52:07 +0800

    feat: edit

--- src/pages/common/template.vue
+++ src/pages/common/template.vue
@@ -74,6 +74,33 @@ onMounted(() =>  {
 
 // plusOne.value = 1
 
+
+const obj = reactive({
+  name: 'zs',
+  address: {
+    info: '北京市',
+    asd: '朝阳区'
+  }
+})
+
+const fullName = computed(() => {
+  return obj.name + '~' + obj.address.info
+}, )
+
+const original = reactive({ count: 0 })
+const copy = readonly(original)
+//立即运行一个函数，同时响应式地追踪其依赖，并在依赖更改时重新执行。
+watchEffect(() => {
+  // 用来做响应性追踪
+  console.log(copy.count)
+})
+// 更改源属性会触发其依赖的侦听器
+original.count++
+// 更改该只读副本将会失败，并会得到一个警告  Cannot assign to 'count' because it is a read-only property
+// copy.count++ // warning!
+
+
+
 </script>
 <style lang='scss' scoped>
 //@import url(); 引入公共css类
